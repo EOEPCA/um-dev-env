5f0cc02d65045f4830deb4a0ef049b439e9c7a8e
Ángel Lozano <angel.lozano@elecnor.es>
2020-03-17 15:56:16 +0100

    add docker, python, git automation

---
+++ provisioning/download-modules.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+git config --global user.name "$1"
+git config --global user.email "$2"
+
+# Allow clone with github as if we know the key already.
+# If this fails, GitHub has their keys compromised.
+cat /home/vagrant/githubKey >> /home/vagrant/.ssh/known_hosts
+rm /home/vagrant/githubKey
+
+# INSERT THE REPOSITORIES IN A LIST (separated by a single space) HERE:
+REPOS="template-svce"
+for i in $REPOS;
+do
+    git clone "git@github.com:EOEPCA/"$i".git" /home/vagrant/$i
+done
