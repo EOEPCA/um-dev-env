afb359b1e42627a205f0e4ee667f91183307d2cb
Ángel Lozano <angel.lozano@elecnor.es>
2020-04-23 19:41:14 +0200

    rename library to well known handler

--- provisioning/download-modules.sh
+++ provisioning/download-modules.sh
@@ -10,7 +10,7 @@ rm /home/vagrant/githubKey
 
 # INSERT THE REPOSITORIES IN A LIST (separated by a single space) HERE:
 TEMPLATES="um-service-template um-pylibrary-template um-buildingblock-template "
-LIBRARIES="um-common-oidc-client um-common-uma-client um-common-scim-client endpoint-lookup" 
+LIBRARIES="um-common-oidc-client um-common-uma-client um-common-scim-client well-known-handler" 
 SERVICES="um-pep-engine um-login-engine um-login-gluu-oxauth um-login-persistence um-user-profile"
 BUILDING_BLOCKS="um-login-service"
 REPOS="$TEMPLATES $LIBRARIES $SERVICES $BUILDING_BLOCKS"
